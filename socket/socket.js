d20fd981540f79693671be087371e8eb41ece945
Aravinthan333 <aravinthan2606@gmail.com>
2024-06-10 07:41:39 +0530

    ....

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["https://symphonious-treacle-81b58f.netlify.app/"],
     methods: ["GET", "POST"],
   },
 });
